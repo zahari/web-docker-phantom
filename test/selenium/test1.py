d4c931cbf551bc40ed5d56e6a1b1a97181965209
Zahari Zahariev <zahari.zahariev@gmail.com>
2016-02-04 10:41:10 +0000

    Run Selenium tests against Browserstack

--- test/selenium/test1.py
+++ test/selenium/test1.py
@@ -3,13 +3,19 @@
 import unittest
 import time
 from selenium import webdriver
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestLocalhost(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.PhantomJS()
+        #self.driver = webdriver.PhantomJS()
         #self.driver = webdriver.Firefox()
+        desired_cap = {'os': 'Windows', 'os_version': '7', 'browser': 'IE', 'browser_version': '8.0' }
+        self.driver = webdriver.Remote(
+            command_executor='http://werwrwr1:nEqWnTfc8Z2Z2wM8kW2p@hub.browserstack.com:80/wd/hub',
+            desired_capabilities=desired_cap)
         self.driver.set_window_size(1120, 550)
 
     def test1_url(self):
