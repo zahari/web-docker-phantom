3c1747dae816caeea297a73e934f1952383b4107
Zahari Zahariev <zahari.zahariev@gmail.com>
2016-02-15 10:39:19 +0000

    Update test1.py

--- test/selenium/test1.py
+++ test/selenium/test1.py
@@ -5,7 +5,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from testingbotclient import TestingBotClient
+#from testingbotclient import TestingBotClient
 
 class TestLocalhost(unittest.TestCase):
 
@@ -14,9 +14,9 @@ class TestLocalhost(unittest.TestCase):
         #driver_local = webdriver.Firefox()
         desired_cap = {'os': 'Windows', 'os_version': '7', 'browser': 'IE', 'browser_version': '8.0' }
         # Testingbot
-        driver_testingbot = webdriver.Remote(
-		    command_executor='http://c3a6a535570605cdab649b40111c83d5:7231f285dc925e9842b8a70e67882ea6@hub.testingbot.com/wd/hub',
-		    desired_capabilities=desired_cap)
+        #driver_testingbot = webdriver.Remote(
+	#	    command_executor='http://c3a6a535570605cdab649b40111c83d5:7231f285dc925e9842b8a70e67882ea6@hub.testingbot.com/wd/hub',
+	#	    desired_capabilities=desired_cap)
         # Browserstack
         driver_browserstack = webdriver.Remote(
             command_executor='http://werwrwr1:nEqWnTfc8Z2Z2wM8kW2p@hub.browserstack.com:80/wd/hub',
@@ -52,8 +52,8 @@ class TestLocalhost(unittest.TestCase):
         self.driver.quit()
         # Testingbot saves test status
         #status = sys.exc_info() == (None, None, None)
-		#tb_client = TestingBotClient('c3a6a535570605cdab649b40111c83d5', '7231f285dc925e9842b8a70e67882ea6')
-		#tb_client.tests.update_test(self.driver.session_id, self._testMethodName, status)
+	#tb_client = TestingBotClient('c3a6a535570605cdab649b40111c83d5', '7231f285dc925e9842b8a70e67882ea6')
+	#tb_client.tests.update_test(self.driver.session_id, self._testMethodName, status)
 
 if __name__ == '__main__':
     unittest.main()
